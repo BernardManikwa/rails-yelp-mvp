a72e88dd6a2bda3ed1c999b5064087bb4c067351
bernardmanikwa <bernard.manikwa@outlook.com>
2024-08-28 17:35:17 +0200

    yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Kumusha = {name: "Kumusha", address: "7 Boundary St, Milnerton", category:"italian"}
+KwaTerry =  {name: "KwaTerry", address: "2 caleb st woodstock", category:"japanese"}
+Yard =  {name: "Yard", address: "2 loop st cape town", category:"belgian"}
+Eiffels =  {name: "Eiffels", address: "2 castle st tableview", category:"french"}
+Blowfish =  {name: "Blowfish", address: "2 waterfront drive V & A", category:"chinese"}
+
+[Kumusha, KwaTerry, Yard, Eiffels] .each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
